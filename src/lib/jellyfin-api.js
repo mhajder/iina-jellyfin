6c4314f127da38afbf66cf9dd2841750ac43de23
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2026-02-27 11:47:44 +0100

    chore: release 0.6.0 (#27)
    * chore: release 0.6.0
    
    * chore: bump ghVersion 7

--- src/lib/jellyfin-api.js
+++ src/lib/jellyfin-api.js
@@ -2,7 +2,7 @@
 
 const CLIENT_NAME = 'IINA Jellyfin Plugin';
 const DEVICE_NAME = 'IINA';
-const CLIENT_VERSION = '0.5.0'; // x-release-please-version
+const CLIENT_VERSION = '0.6.0'; // x-release-please-version
 
 function createJellyfinApi({ http, preferences, log }) {
   function getDeviceId() {
